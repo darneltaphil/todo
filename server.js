005eaf326411a622175c577305af612a8deb5cd8
darneltaphil <darneltaphil@gmail.com>
2020-11-11 16:05:27 +0000

    Rediection added

--- server.js
+++ server.js
@@ -6,4 +6,10 @@ app.use(express.static(path.join(__dirname,'build')));
 app.get("/", (req,res)=>{
     res.sendFile(path.join(__dirname,'build', 'index.html'))
 })
+
+//Redirecting any other URL to root 
+app.get('*', (req, res) => {
+    res.redirect('/')
+})
+
 app.listen(5000);
